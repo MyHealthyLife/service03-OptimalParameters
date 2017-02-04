86d21ae495450f98cb0887e7635cfc2a255f2450
StefanoTavonatti93 <stefano.tavonatti@studenti.unitn.it>
2017-02-04 12:25:41 +0100

    JPA and Dao

--- service03-OptimalParameters/src/myhealtylife/optimalparamters/model/entity/AgeRange.java
+++ service03-OptimalParameters/src/myhealtylife/optimalparamters/model/entity/AgeRange.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -13,8 +16,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name="age_range")
-@NamedQuery(name="age_range.findAll", query="SELECT a FROM age_range a")
-@XmlRootElement(name="person")
+@NamedQuery(name="AgeRange.findAll", query="SELECT a FROM AgeRange a")
+@XmlRootElement(name="age_range")
 public class AgeRange implements Serializable{
 
 	/**
@@ -22,11 +25,47 @@ public class AgeRange implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id // defines this attributed as the one that identifies the entity
+    @GeneratedValue(strategy=GenerationType.AUTO) 
+	private long idRange;
+	
 	private Integer fromAge;
 	
 	private Integer toAge;
 	
 	@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL,mappedBy="ageRange")
 	private List<Parameter> parameter;
+
+	public long getIdRange() {
+		return idRange;
+	}
+
+	public void setIdRange(long idRange) {
+		this.idRange = idRange;
+	}
+
+	public Integer getFromAge() {
+		return fromAge;
+	}
+
+	public void setFromAge(Integer fromAge) {
+		this.fromAge = fromAge;
+	}
+
+	public Integer getToAge() {
+		return toAge;
+	}
+
+	public void setToAge(Integer toAge) {
+		this.toAge = toAge;
+	}
+
+	public List<Parameter> getParameter() {
+		return parameter;
+	}
+
+	public void setParameter(List<Parameter> parameter) {
+		this.parameter = parameter;
+	}
 
 }
--- service03-OptimalParameters/src/myhealtylife/optimalparamters/model/entity/Parameter.java
+++ service03-OptimalParameters/src/myhealtylife/optimalparamters/model/entity/Parameter.java
@@ -15,7 +15,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name="parameters")
-@NamedQuery(name="Parameter.findAll", query="SELECT p FROM parameters p")
+@NamedQuery(name="Parameter.findAll", query="SELECT p FROM Parameter p")
 @XmlRootElement(name="parameter")
 public class Parameter implements Serializable{
 
@@ -36,5 +36,45 @@ public class Parameter implements Serializable{
 	
 	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
 	private AgeRange ageRange;
+
+	public long getIdParameter() {
+		return idParameter;
+	}
+
+	public void setIdParameter(long idParameter) {
+		this.idParameter = idParameter;
+	}
+
+	public String getParameterName() {
+		return parameterName;
+	}
+
+	public void setParameterName(String parameterName) {
+		this.parameterName = parameterName;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public AgeRange getAgeRange() {
+		return ageRange;
+	}
+
+	public void setAgeRange(AgeRange ageRange) {
+		this.ageRange = ageRange;
+	}
 
 }
