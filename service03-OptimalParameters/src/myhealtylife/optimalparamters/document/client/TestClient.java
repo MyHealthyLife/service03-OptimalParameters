707c0e365fb7f6047840746a9a14ba8b1f29bf5e
StefanoTavonatti93 <stefano.tavonatti@studenti.unitn.it>
2017-02-05 14:17:26 +0100

    comments

--- service03-OptimalParameters/src/myhealtylife/optimalparamters/document/client/TestClient.java
+++ service03-OptimalParameters/src/myhealtylife/optimalparamters/document/client/TestClient.java
@@ -17,7 +17,7 @@ public class TestClient {
 	
 	public static void main(String args[]) throws MalformedURLException{
 		URL url = new URL("http://127.0.1.1:6901/ws/optimal_paramters?wsdl");
-	  	//URL url = new URL("https://service01-dataservice.herokuapp.com/ws/data_service?wsdl");
+	  	//URL url = new URL("https://service03-optimalparameters.herokuapp.com/ws/optimal_paramters?wsdl");
         //1st argument service URI, refer to wsdl document above
         //2nd argument is service name, refer to wsdl document above
         QName qname = new QName("http://soap.optimalparamters.myhealtylife/", "OptimalParamteres");
--- service03-OptimalParameters/src/myhealtylife/optimalparamters/soap/OptimalParameters.java
+++ service03-OptimalParameters/src/myhealtylife/optimalparamters/soap/OptimalParameters.java
@@ -18,42 +18,93 @@ public interface OptimalParameters {
 
 	@WebMethod(operationName="readOptimalParameters")
 	@WebResult(name="parametersList")
+	/**
+	 * get the list of available parameters.
+	 * @return
+	 */
 	public ParametersList readOptimalParameters();
 	
 	@WebMethod(operationName="readOptimalParametersByAgeAndSex")
 	@WebResult(name="parametersList")
+	/**
+	 * returns a list of Parameter object filtered by sex and age
+	 * @param age
+	 * @param sex "M" for male of "F" for female
+	 * @return
+	 */
 	public ParametersList readOptimalParametersByAgeAndSex(Integer age, String sex);
 	
 	@WebMethod(operationName="readOptimalParametersByNameAgeAndSex")
 	@WebResult(name="parametersList")
+	/**
+	 * returns a list of Parameter object filtered by sex, age and name of the parameter.
+	 * @param age
+	 * @param parameterName
+	 * @param sex
+	 * @return
+	 */
 	public ParametersList readOptimalParametersByNameAgeAndSex(Integer age,String parameterName ,String sex);
 	
 	@WebMethod(operationName="createParameter")
 	@WebResult(name="parameter")
+	/**
+	 * Save a new parameter into the database.
+	 * @param p the parameter to save.
+	 * @return
+	 */
 	public Parameter createParameter(Parameter p);
 	
 	@WebMethod(operationName="updateParameter")
 	@WebResult(name="parameter")
+	/**
+	 * Update an already existing parameter
+	 * @param p parameter to update
+	 * @return
+	 */
 	public Parameter updateParameter(Parameter p);
 	
 	@WebMethod(operationName="deleteParameter")
 	@WebResult(name="idParameter")
+	/**
+	 * delete a parameter from database
+	 * @param parameterId
+	 * @return
+	 */
 	public long deleteParameter(long parameterId);
 	
 	@WebMethod(operationName="readAgeRanges")
 	@WebResult(name="AgeRangeList")
+	/**
+	 * returns the list of available AgeRange
+	 * @return
+	 */
 	public AgeRangeList readAgeRanges();
 	
 	@WebMethod(operationName="createAgeRange")
 	@WebResult(name="AgeRange")
+	/**
+	 * create a new AgeRange
+	 * @param ag AgeRange which will be saved into the database
+	 * @return
+	 */
 	public AgeRange createAgeRange(AgeRange ag);
 	
 	@WebMethod(operationName="updateAgeRange")
 	@WebResult(name="AgeRange")
+	/**
+	 * update an already existing AgeRange
+	 * @param ag AgeRange which will be updated
+	 * @return
+	 */
 	public AgeRange updateAgeRange(AgeRange ag);
 	
 	@WebMethod(operationName="deleteAgeRange")
 	@WebResult(name="idAgeRange")
+	/**
+	 * delete an AgeRange
+	 * @param ageRangeId
+	 * @return
+	 */
 	public long deleteAgeRange(long ageRangeId);
 	
 	
